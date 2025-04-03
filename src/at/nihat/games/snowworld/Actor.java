75910413cfb33dd26e78267ed5dd9121546368cd
nihat_08 <nihat.oezgoec@student.htldornbirn.at>
2025-04-03 09:27:31 +0200

    OO Games end of lesson

---
+++ src/at/nihat/games/snowworld/Actor.java
@@ -0,0 +1,9 @@
+package at.nihat.games.snowworld;
+
+import org.newdawn.slick.Graphics;
+
+public interface Actor {
+    public void render(Graphics graphics);
+    public void update(int render);
+
+}
---
+++ src/at/nihat/games/snowworld/Snowflake.java
@@ -0,0 +1,39 @@
+package at.nihat.games.snowworld;
+
+import org.newdawn.slick.Graphics;
+
+import java.util.Random;
+
+public class Snowflake implements Actor{
+    public enum SIZE {BIG, SMALL, MEDIUM};
+    private float x,y;
+    private int size;
+    private int speed;
+    private Random random;
+
+    public Snowflake(SIZE size){
+        this.random = new Random();
+        if (size==SIZE.BIG){
+            this.size = 12;
+            this.speed = 2;
+        }
+        if (size==SIZE.MEDIUM){
+            this.size = 8;
+            this.speed = 5;
+        }
+        if (size==SIZE.SMALL){
+            this.size = 5;
+            this.speed = 9;
+        }
+    }
+
+    @Override
+    public void render(Graphics graphics) {
+
+    }
+
+    @Override
+    public void update(int render) {
+
+    }
+}
