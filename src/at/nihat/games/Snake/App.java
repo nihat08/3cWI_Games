b57875b5c1187fb335c7d455647434dd145bae11
nihat_08 <nihat.oezgoec@student.htldornbirn.at>
2025-06-12 08:53:04 +0200

    OO Snake Fenster erstellt

---
+++ src/at/nihat/games/Snake/App.java
@@ -0,0 +1,17 @@
+package at.nihat.games.Snake;
+
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+        int boardWidth = 600;
+        int boardHeight = boardWidth;
+
+        JFrame frame = new JFrame("Snake");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
